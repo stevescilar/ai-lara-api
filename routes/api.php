335813fe0482aee43d72542987f0bf5d01447bcf
Stephen Muambi <stevemuambi@gmail.com>
2025-12-17 11:27:34 +0300

    Postcontroller API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -9,4 +10,9 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/hello', function () {
     return response()->json(['message' => 'Hello, World!']);
+});
+
+//API Version 1 Routes
+Route::prefix('v1')->group(function () {
+    Route::apiResource('posts', PostController::class); // Registers standard RESTful routes for Post resource
 });
